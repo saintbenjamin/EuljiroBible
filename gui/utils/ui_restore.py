17a0b9bec186cb83d353526c6e63e776a596828b
Benjamin J. Choi <benchoi@ccs.tsukuba.ac.jp>
2026-01-03 23:27:32 +0900

    update RTD v3,1

--- gui/utils/ui_restore.py
+++ gui/utils/ui_restore.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 :File: EuljiroBible/gui/utils/ui_restore.py
-
-Restores user settings into the main window UI components.
-
 :Author: Benjamin Jaedon Choi - https://github.com/saintbenjamin
 :Affiliated Church: The Eulji-ro Presbyterian Church [대한예수교장로회(통합) 을지로교회]
 :Address: The Eulji-ro Presbyterian Church, 24-10, Eulji-ro 20-gil, Jung-gu, Seoul 04549, South Korea
 :Telephone: +82-2-2266-3070
 :E-mail: euljirochurch [at] G.M.A.I.L. (replace [at] with @ and G.M.A.I.L as you understood.)
 :License: MIT License with Attribution Requirement (see LICENSE file for details); Copyright (c) 2025 The Eulji-ro Presbyterian Church.
+
+Restores user settings into the main window UI components.
 """
 
 def restore_settings_to_ui(win, settings):
