2c3cf4d565e86b0b5d18d33db5314baa58448bb5
Benjamin J. Choi <benchoi@ccs.tsukuba.ac.jp>
2026-01-04 10:52:20 +0900

    update RTD v5

--- EuljiroBible.py
+++ EuljiroBible.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 """
 :File: EuljiroBible/EuljiroBible.py
-
-Main launcher script for the GUI version of EuljiroBible.
-
-This script serves as the entry point when running EuljiroBible directly.
-It delegates the actual initialization and application setup to the
-`gui_main.py` module.
-
 :Author: Benjamin Jaedon Choi - https://github.com/saintbenjamin
 :Affiliated Church: The Eulji-ro Presbyterian Church [대한예수교장로회(통합) 을지로교회]
 :Address: The Eulji-ro Presbyterian Church, 24-10, Eulji-ro 20-gil, Jung-gu, Seoul 04549, South Korea
 :Telephone: +82-2-2266-3070
 :E-mail: euljirochurch [at] G.M.A.I.L. (replace [at] with @ and G.M.A.I.L as you understood.)
 :License: MIT License with Attribution Requirement (see LICENSE file for details); Copyright (c) 2025 The Eulji-ro Presbyterian Church.
+
+Main launcher script for the GUI version of EuljiroBible.
+
+This script serves as the entry point when running EuljiroBible directly.
+It delegates the actual initialization and application setup to the
+`gui_main.py` module.
 """
 
 from gui.gui_main import main as gui_main
